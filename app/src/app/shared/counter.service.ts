1baaf292cf2f46e97216b91c1279afce77bbda37
Antonioverrastro <smic99@gmail.com>
2021-12-09 19:25:42 +0100

    Mancano le ultime aggiustatine

--- app/src/app/shared/counter.service.ts
+++ app/src/app/shared/counter.service.ts
@@ -5,24 +5,26 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class CounterService {
-  private counterSubject = new BehaviorSubject<number>(100);
+  private counterSubject = new BehaviorSubject<number>(0);
   public counter$ = this.counterSubject.asObservable();
 
   private counter: number = 0;
   default = 1;
 
   constructor() {}
-  show(): Observable<number> | string {
-    if (this.counterSubject.value < 0) {
-      return 'error';
+  show() {
+    if (this.counterSubject.value > 0) {
+      return this.counterSubject;
     } else {
-      return this.counter$;
+      return 'error';
     }
   }
   add(value: number = 1): number {
+    this.counterSubject.next(value += this.counterSubject.value);
     return (this.counter += value);
   }
   remove(value: number = 1): number {
+    this.counterSubject.next(this.counterSubject.value - value);
     return (this.counter -= value);
   }
 }
